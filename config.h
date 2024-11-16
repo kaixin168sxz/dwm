177c308cc7de5920372af3abd3d069577ff1936d
宋昕哲 <kaixin@ArchAMD.localdomain>
2024-11-16 10:32:49 +0800

    将dmenu的颜色变量改为nord

--- config.h
+++ config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_nordbg, "-nf", col_nordfg, "-sb", col_nordbgSel, "-sf", col_nordfgSel, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *fmcmd[]  = { "thunar", NULL };
 static const char *webcmd[]  = { "chromium", NULL };
